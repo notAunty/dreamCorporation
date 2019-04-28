01a89b69f227e20dee6b378d8b54b8ced4547397
wid180039 <wid180039+github@siswa.um.edu.my>
2019-04-28 23:54:57 +0800

    initial

--- app/src/main/java/endgame/data/dreamcorporation/SignupActivity.java
+++ app/src/main/java/endgame/data/dreamcorporation/SignupActivity.java
@@ -3,7 +3,10 @@ package endgame.data.dreamcorporation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,19 +21,47 @@ public class SignupActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_signup);
 
-//    Button login_button = (Button) findViewById(R.id.login_button);
     TextView login = (TextView) findViewById(R.id.to_login_screen);
-//    String userId = (String) findViewById(R.)
+    final String userId = ((EditText) findViewById(R.id.signup_userid)).getText().toString();
+    final String userPw = ((EditText) findViewById(R.id.signup_userpw)).getText().toString();
+    Button signup_button = (Button) findViewById(R.id.signup_button);
+    mAuth = FirebaseAuth.getInstance(); // Initialize Firebase Auth
 
-//    login_button.setOnClickListener(new View.OnClickListener(){
-//      @Override
-//      //On click function
-//      public void onClick(View view) {
+    signup_button.setOnClickListener(new View.OnClickListener(){
+      @Override
+      //On click function
+      public void onClick(final View view) {
+
+        Toast.makeText(view.getContext(), "Authentication failed.",
+                Toast.LENGTH_SHORT).show();
+
 //        //Create the intent to start another activity
 //        Intent intent = new Intent(view.getContext(), HomeActivity.class);
 //        startActivity(intent);
-//      }
-//    });
+
+
+//        mAuth.createUserWithEmailAndPassword(userId + "@gmail.com", userPw)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                  @Override
+//                  public void onComplete(@NonNull Task<AuthResult> task) {
+//                    if (task.isSuccessful()) {
+//                      // Sign in success, update UI with the signed-in user's information
+//                      Log.d("signup: ", "createUserWithEmail:success");
+//                      FirebaseUser user = mAuth.getCurrentUser();
+//                      updateUI(user);
+//                    } else {
+//                      // If sign in fails, display a message to the user.
+//                      Log.w("signup: ", "createUserWithEmail:failure", task.getException());
+//                      Toast.makeText(view.getContext(), "Authentication failed.",
+//                              Toast.LENGTH_SHORT).show();
+//                      updateUI(null);
+//                    }
+//
+//                    // ...
+//                  }
+//                });
+      }
+    });
 
     login.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -41,29 +72,5 @@ public class SignupActivity extends AppCompatActivity {
         startActivity(intent);
       }
     });
-
-    // Initialize Firebase Auth
-//    mAuth = FirebaseAuth.getInstance();
-
-//    mAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//              @Override
-//              public void onComplete(@NonNull Task<AuthResult> task) {
-//                if (task.isSuccessful()) {
-//                  // Sign in success, update UI with the signed-in user's information
-//                  Log.d(TAG, "createUserWithEmail:success");
-//                  FirebaseUser user = mAuth.getCurrentUser();
-//                  updateUI(user);
-//                } else {
-//                  // If sign in fails, display a message to the user.
-//                  Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                  Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                          Toast.LENGTH_SHORT).show();
-//                  updateUI(null);
-//                }
-//
-//                // ...
-//              }
-//            });
   }
 }
