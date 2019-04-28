e2c3013023141785eb4f222e220456129ec0d9f5
wid180039 <wid180039+github@siswa.um.edu.my>
2019-04-28 17:39:45 +0800

    initial

---
+++ app/src/main/java/endgame/data/dreamcorporation/LoginActivity.java
@@ -0,0 +1,17 @@
+package endgame.data.dreamcorporation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
