924c79fe8774ab33431c85f985c05749ea2a2bcd
wid180039 <wid180039+github@siswa.um.edu.my>
2019-04-29 12:12:22 +0800

    initial

--- app/src/main/java/endgame/data/dreamcorporation/SignupActivity.java
+++ app/src/main/java/endgame/data/dreamcorporation/SignupActivity.java
@@ -42,30 +42,35 @@ public class SignupActivity extends AppCompatActivity {
 //        Intent intent = new Intent(view.getContext(), HomeActivity.class);
 //        startActivity(intent);
 
-
-        mAuth.createUserWithEmailAndPassword(userId + "@gmail.com", userPw)
-                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                  @Override
-                  public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                      // Sign in success, update UI with the signed-in user's information
-                      Log.d("signup: ", "createUserWithEmail:success");
-                      FirebaseUser user = mAuth.getCurrentUser();
-                      Toast.makeText(view.getContext(), "Signup success!",
-                              Toast.LENGTH_SHORT).show();
-                      Intent intent = new Intent(view.getContext(), MainActivity.class);
-                      startActivity(intent);
-                      onBackPressed();
-                    } else {
-                      // If sign in fails, display a message to the user.
-                      Log.w("signup: ", "createUserWithEmail:failure", task.getException());
-                      Toast.makeText(view.getContext(), "Signup failed.",
-                              Toast.LENGTH_SHORT).show();
+        if (userPw.length() < 8) {
+          Toast.makeText(view.getContext(), "Password longer a bit can ah??",
+                  Toast.LENGTH_SHORT).show();
+        } else {
+          mAuth.createUserWithEmailAndPassword(userId + "@gmail.com", userPw)
+                  .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                      if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("signup: ", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(view.getContext(), "Signup success! You may login now.",
+                                Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                        startActivity(intent);
+                        onBackPressed();
+                      } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("signup: ", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(view.getContext(), "Signup failed.",
+                                Toast.LENGTH_SHORT).show();
 //                      updateUI(null);
+                      }
+                      // ...
                     }
-                    // ...
-                  }
-                });
+                  });
+        }
+
         onBackPressed(); // To close this activity
       }
     });
