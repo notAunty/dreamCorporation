68e1a780e8da24981671746268931222c210d93c
wid180039 <wid180039+github@siswa.um.edu.my>
2019-04-29 14:31:55 +0800

    initial

--- app/src/main/java/endgame/data/dreamcorporation/MainActivity.java
+++ app/src/main/java/endgame/data/dreamcorporation/MainActivity.java
@@ -2,13 +2,21 @@ package endgame.data.dreamcorporation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,17 +29,40 @@ public class MainActivity extends AppCompatActivity {
 
     Button login_button = (Button) findViewById(R.id.login_button);
     TextView signup = (TextView) findViewById(R.id.to_signup_screen);
+    mAuth = FirebaseAuth.getInstance(); // Initialize Firebase Auth
 
 
     login_button.setOnClickListener(new View.OnClickListener() {
       @Override
       //On click function
-      public void onClick(View view) {
-        //Create the intent to start another activity
-        Intent intent = new Intent(view.getContext(), HomeActivity.class);
-        startActivity(intent);
+      public void onClick(final View view) {
 
-        onBackPressed(); // To close this activity
+        String userId = ((EditText) findViewById(R.id.login_userid)).getText().toString();
+        String userPw = ((EditText) findViewById(R.id.login_userpw)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(userId + "@gmail.com", userPw)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                  @Override
+                  public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                      // Sign in success, update UI with the signed-in user's information
+                      Log.d("login: ", "signInWithEmail:success");
+                      FirebaseUser user = mAuth.getCurrentUser();
+                      //Create the intent to start another activity
+                      Intent intent = new Intent(view.getContext(), HomeActivity.class);
+                      startActivity(intent);
+                    } else {
+                      // If sign in fails, display a message to the user.
+                      Log.w("login: ", "signInWithEmail:failure", task.getException());
+                      Toast.makeText(view.getContext(), "Username or password incorrect.",
+                              Toast.LENGTH_SHORT).show();
+//                      updateUI(null);
+                    }
+                    // ...
+                  }
+                });
+
+      onBackPressed(); // To close this activity
       }
     });
 
