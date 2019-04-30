b685f9af2e13b088ba94caedeecd1d480d9b73c8
Wei Peng <tlbb2016@hotmail.com>
2019-04-30 15:16:10 +0800

    test1

--- app/src/main/java/endgame/data/dreamcorporation/NetworkFragment.java
+++ app/src/main/java/endgame/data/dreamcorporation/NetworkFragment.java
@@ -13,6 +13,7 @@ public class NetworkFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_network, container, false);
+    View view = inflater.inflate(R.layout.fragment_network, container, false);
+    return view;
   }
 }
