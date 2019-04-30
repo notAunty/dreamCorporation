932d07e80af2c2116bac73eac1d2970847faa174
wid180039 <wid180039+github@siswa.um.edu.my>
2019-04-30 12:34:09 +0800

    show toast when login success

--- app/src/main/java/endgame/data/dreamcorporation/ProfileFragment.java
+++ app/src/main/java/endgame/data/dreamcorporation/ProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,6 +19,11 @@ public class ProfileFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_profile, container, false);
+    View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+    TextView fullName = (TextView) view.findViewById(R.id.profile_name);
+    String tempUID = mAuth.getUid();
+
+    if (!tempUID.isEmpty()) fullName.setText(tempUID);
   }
 }
