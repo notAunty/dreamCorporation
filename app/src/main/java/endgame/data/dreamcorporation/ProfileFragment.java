c79587eac00df1a3891276349d9ef93581d470ef
wid180039 <wid180039+github@siswa.um.edu.my>
2019-05-01 17:30:41 +0800

    Fixed profile decrypt dialog

--- app/src/main/java/endgame/data/dreamcorporation/ProfileFragment.java
+++ app/src/main/java/endgame/data/dreamcorporation/ProfileFragment.java
@@ -1,20 +1,17 @@
 package endgame.data.dreamcorporation;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.media.Image;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,52 +20,91 @@ public class ProfileFragment extends Fragment {
 
   private FirebaseAuth mAuth;
   final Context context = getActivity();
-  private TextView textView;
-
+  private TextView decrypt;
+  private String key;
 
   @Nullable
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
     mAuth = FirebaseAuth.getInstance(); // Initialize Firebase Auth // IMPORTANT
     View view = inflater.inflate(R.layout.fragment_profile, container, false);
+//    final FragmentManager manager = getFragmentManager();
+
 
     TextView uidField = (TextView) view.findViewById(R.id.uid);
     String tempUID = mAuth.getUid();
-
     if (!tempUID.isEmpty()) uidField.setText(tempUID);
 
-    textView = (TextView) view.findViewById(R.id.testing);
+    decrypt = (TextView) view.findViewById(R.id.profile_name);
 
-    ImageView imageView = (ImageView) view.findViewById(R.id.opendialog);
-    imageView.setOnClickListener(new View.OnClickListener() {
+    decrypt.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        LayoutInflater layoutInflater = LayoutInflater.from(context);
-        View temp = layoutInflater.inflate(R.layout.dialog,null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setView(temp);
-        final EditText editText = (EditText)temp.findViewById(R.id.Text);
-        builder.setCancelable(false).setPositiveButton("OK",new DialogInterface.OnClickListener() {
-          public void onClick(DialogInterface dialog,int id) {
-          // get user input and set it to result
-            // edit text
-            textView.setText(editText.getText());
-          }
-        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-          public void onClick(DialogInterface dialog,int id) {
-            dialog.cancel();
-          }
-        });
+//        keyDialog.show(getFragmentManager(), "a");
 
-        // create alert dialog
-        AlertDialog alertDialog = builder.create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
-        // show it
-        alertDialog.show();
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+//        builder.setTitle("Title");
+//        builder.setMessage("Do you want to delete ?");
+
+        builder.setView(inflater.inflate(R.layout.dialog_key, null))
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                  public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getContext(),
+                            "Click-Click!!", Toast.LENGTH_LONG).show();
+                    dialog.cancel();
+                  }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+//        LayoutInflater layoutInflater = LayoutInflater.from(context);
+//        View temp = layoutInflater.inflate(R.layout.dialog,null);
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.setView(temp);
+//        final EditText editText = (EditText)temp.findViewById(R.id.Text);
+//        builder.setCancelable(false).setPositiveButton("OK",new DialogInterface.OnClickListener() {
+//          public void onClick(DialogInterface dialog,int id) {
+//          // get user input and set it to result
+//            // edit text
+//            textView.setText(editText.getText());
+//          }
+//        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//          public void onClick(DialogInterface dialog,int id) {
+//            dialog.cancel();
+//          }
+//        });
+//
+//        // create alert dialog
+//        AlertDialog alertDialog = builder.create();
+//
+//        // show it
+//        alertDialog.show();
       }
     });
 
     return view;
   }
 
+//  public void onCreateDialog() {
+//    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//    // Get the layout inflater
+//    LayoutInflater inflater = requireActivity().getLayoutInflater();
+//
+//    // Inflate and set the layout for the dialog
+//    // Pass null as the parent view because its going in the dialog layout
+//    builder.setView(inflater.inflate(R.layout.dialog_key, null))
+//            // Add action buttons
+//            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//              @Override
+//              public void onClick(DialogInterface dialog, int id) {
+////                key = ((EditText) getActivity().findViewById(R.id.login_userid)).getText().toString();
+//              }
+//            });
+//    AlertDialog dialog = builder.create();
+//    dialog.create();
+////    return builder.create();
+//  }
 }
