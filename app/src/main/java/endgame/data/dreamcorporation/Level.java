24d3e92e392791eb24784b91cd3dad70934e0d1d
wid180039 <wid180039+github@siswa.um.edu.my>
2019-05-09 22:52:38 +0800

    NETWORK SCREEN DONE
    but decrption got problem... so....

--- app/src/main/java/endgame/data/dreamcorporation/Level.java
+++ app/src/main/java/endgame/data/dreamcorporation/Level.java
@@ -1,22 +1,22 @@
-package endgame.data.dreamcorporation;
-
-public class Level {
-  public Node head = null;
-  public Node tail = null;
-  public boolean haveChild = false;
-
-  public void add(String e, boolean haveChild) {
-    Node newNode = new Node(e);
-    if (head == null) {
-      head = newNode;
-      tail = newNode;
-    } else {
-      tail.next = newNode;
-      tail = newNode;
-    }
-  }
-
-//  public void push(String o) {
-//    array.add(o);
+//package endgame.data.dreamcorporation;
+//
+//public class Level {
+//  public Node head = null;
+//  public Node tail = null;
+////  public boolean haveChild;
+//
+//  public void add(String e, boolean haveDownline) {
+//    Node newNode = new Node(e, haveDownline);
+//    if (head == null) {
+//      head = newNode;
+//      tail = newNode;
+//    } else {
+////      tail.next = newNode;
+//      tail = newNode;
+//    }
 //  }
-}
+//
+////  public void push(String o) {
+////    array.add(o);
+////  }
+//}
--- app/src/main/java/endgame/data/dreamcorporation/Node.java
+++ app/src/main/java/endgame/data/dreamcorporation/Node.java
@@ -1,15 +1,48 @@
-package endgame.data.dreamcorporation;
-
-import java.util.ArrayList;
-
-public class Node<String> {
-
-  protected ArrayList<String> element;
-  protected Node<String> next;
-
-  public Node() {}
-
-  public Node(String element) {
-    this.element.add(element);
-  }
-}
+//package endgame.data.dreamcorporation;
+//
+//import java.util.ArrayList;
+//
+//public class Node<String> {
+//
+//  protected String uid;
+//  protected ArrayList<String> downlines;
+//  protected ArrayList<Boolean> gotDownlines;
+//  protected ArrayList<Node> downlinesNode;
+//
+//  public Node(String uid, ArrayList<String> downlines, ArrayList<Boolean> gotDownlines) {
+//    this.uid = uid;
+//    this.downlines = downlines;
+//    this.gotDownlines = gotDownlines;
+//  }
+//
+//  public Node(String uid, ArrayList<String> downlines) {
+//    this.uid = uid;
+//    this.downlines = downlines;
+//  }
+//
+//  public Node(String uid) {
+//    this.uid = uid;
+//  }
+//
+//  public void addBool(String downlines, boolean gotDownlines) {
+//    this.gotDownlines.add(this.downlines.indexOf(downlines), gotDownlines);
+//  }
+//
+//  public ArrayList<Node> getDownlinesNode() {
+//    return downlinesNode;
+//  }
+//
+//  public void addDownlinesNode(Node downlinesNode) {
+//    this.downlinesNode.add(downlinesNode);
+//  }
+//
+//  public String getUid() {return uid;}
+//
+//  //  public void compile() {
+////    int size = downlines.size();
+////    downlinesNode = new Node[size];
+////    for (int i = 0; i < size; i++) {
+////      downlinesNode[i] = new Node(this.downlines.get(i));
+////    }
+////  }
+//}
